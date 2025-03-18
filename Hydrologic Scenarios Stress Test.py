c6783dcb209a97c58c045eebb110a4ffb0ba15d7
Anabelle374 <anabellemyers44@gmail.com>
2025-03-18 15:12:34 -0600

    Anabelle removed errors in code

--- Hydrologic Scenarios Stress Test.py
+++ Hydrologic Scenarios Stress Test.py
@@ -1,15 +1,12 @@
 from urllib.request import install_opener
 import pandas as pd
-<<<<<<< HEAD
 import openpyxl
 import numpy as np
 
 print(openpyxl.__version__)
-=======
 # Add Comment
 # David added comment
 # David added a second comment March 18, 2025
->>>>>>> b3b7946985f85ee2d5107854c6a1528d741c5050
 # Read Excel file sheet: ISM_StressTest
 stress_test = pd.read_excel('C:/Users/A02369941/Downloads/HydrologyScenarios.xlsx', sheet_name = 'ISM_StressTest')
 # Display data
